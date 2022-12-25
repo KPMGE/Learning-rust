4355e383fa417e298a6c2b2f0d9badf2075d82d3
KPMGE <jesuskevin254@gmail.com>
2022-12-25 19:08:12 -0300

    :sparkles: add cors support

--- backend/blog_api/src/main.rs
+++ backend/blog_api/src/main.rs
@@ -2,7 +2,8 @@ use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 use serde::Serialize;
 
-use warp::hyper::StatusCode;
+use warp::cors::CorsForbidden;
+use warp::hyper::{StatusCode, Method};
 use warp::{Filter, Rejection, Reply};
 use warp::reject::Reject;
 
@@ -44,10 +45,12 @@ struct InvalidId;
 impl Reject for InvalidId {}
 
 async fn invalid_id_error_handler(r: Rejection) -> Result<impl Reply, Rejection> {
-  if let Some(InvalidId) = r.find() {
-    Ok(warp::reply::with_status("No valid id presented", StatusCode::UNPROCESSABLE_ENTITY))
+  if let Some(error) = r.find::<CorsForbidden>() {
+    Ok(warp::reply::with_status(error.to_string(), StatusCode::FORBIDDEN))
+  } else if let Some(InvalidId) = r.find() {
+    Ok(warp::reply::with_status("No valid id presented".to_string(), StatusCode::UNPROCESSABLE_ENTITY))
   } else {
-    Ok(warp::reply::with_status("Route not found", StatusCode::NOT_FOUND))
+    Ok(warp::reply::with_status("Route not found".to_string(), StatusCode::NOT_FOUND))
   }
 }
 
@@ -70,13 +73,18 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
 
 #[tokio::main]
 async fn main() {
+  let cors = warp::cors()
+    .allow_any_origin()
+    .allow_header("content-type")
+    .allow_methods(&[Method::PUT, Method::DELETE]);
+
   let get_questions_route = warp::get()
     .and(warp::path("questions"))
     .and(warp::path::end())
     .and_then(get_questions)
     .recover(invalid_id_error_handler);
 
-  let routes = get_questions_route;
+  let routes = get_questions_route.with(cors);
 
   println!("Listening on: http://locahost:3333");
   warp::serve(routes).run(([127, 0, 0, 1], 3333)).await;
