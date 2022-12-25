0b9745e74af93c35e1495580ca0dadd4ce6bb7aa
KPMGE <jesuskevin254@gmail.com>
2022-12-25 13:03:20 -0300

    :sparkles: create Question struct

--- backend/blog_api/src/main.rs
+++ backend/blog_api/src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Debug)]
+struct QuestionId(String);
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+  let q1 = Question::new(
+    QuestionId("1234".to_string()),
+    "What's the answer for the life, the universe and everything?".to_string(),
+    "some content".to_string(),
+    Some(vec!["faq".to_string(), "fiction".to_string()]),
+  );
+
+  println!("{:?}", q1);
 }
