3cc5d09605d915322c8eeec8df86a668d8015e0a
KPMGE <jesuskevin254@gmail.com>
2023-01-08 13:24:18 -0300

    :sparkles: integrate profanity check through an external api

--- backend/blog_api/error-handler/src/lib.rs
+++ backend/blog_api/error-handler/src/lib.rs
@@ -3,15 +3,31 @@ use warp::reject::Reject;
 use warp::cors::CorsForbidden;
 use warp::body::BodyDeserializeError;
 use warp::hyper::StatusCode;
+use reqwest::Error as ReqwestError;
 
-use tracing::{event, Level, instrument};
+use tracing::{event, Level};
 use std::fmt;
+
+#[derive(Debug, Clone)]
+pub struct ApiLayerError {
+  pub status: u16,
+  pub message: String
+}
+
+impl std::fmt::Display for ApiLayerError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(f, "Status: {}, Message: {}", self.status, self.message)
+  }
+}
 
 #[derive(Debug)]
 pub enum ApiError {
   ParseError(std::num::ParseIntError),
   MissingParamError,
   DatabaseQueryError,
+  ExternalApiError(ReqwestError),
+  ClientError(ApiLayerError),
+  ServerError(ApiLayerError),
 }
 
 impl std::fmt::Display for ApiError {
@@ -19,24 +35,50 @@ impl std::fmt::Display for ApiError {
     match &*self {
       ApiError::ParseError(ref err) => writeln!(f, "could not parse parameter: {}", err),
       ApiError::MissingParamError => writeln!(f, "missing parameter"), 
-      ApiError::DatabaseQueryError => write!(f, "cannot update, invalid data.")
+      ApiError::DatabaseQueryError => write!(f, "cannot update, invalid data."),
+      ApiError::ExternalApiError(err) => write!(f, "cannot execute: {}", err),
+      ApiError::ClientError(err) => write!(f, "external client error: {}", err),
+      ApiError::ServerError(err) => write!(f, "external server error: {}", err),
     }
   }
 }
 
 impl Reject for ApiError {}
+impl Reject for ApiLayerError {}
 
 pub async fn handle_errors(r: Rejection) -> Result<impl Reply, Rejection> {
   if let Some(crate::ApiError::DatabaseQueryError) = r.find() {
-    event!(Level::ERROR, "databasse query error");
-    Ok(warp::reply::with_status(crate::ApiError::DatabaseQueryError.to_string(), StatusCode::UNPROCESSABLE_ENTITY))
+    event!(Level::ERROR, "database query error");
+    Ok(warp::reply::with_status(
+      crate::ApiError::DatabaseQueryError.to_string(), 
+      StatusCode::UNPROCESSABLE_ENTITY
+    ))
   } else if let Some(error) = r.find::<CorsForbidden>() {
-    Ok(warp::reply::with_status(error.to_string(), StatusCode::FORBIDDEN))
+    event!(Level::ERROR, "CORS forbidden error: {}", error);
+    Ok(warp::reply::with_status(
+      error.to_string(),
+      StatusCode::FORBIDDEN
+    ))
   } else if let Some(error) = r.find::<ApiError>() {
-    Ok(warp::reply::with_status(error.to_string(), StatusCode::RANGE_NOT_SATISFIABLE))
+    Ok(warp::reply::with_status(
+      error.to_string(),
+      StatusCode::RANGE_NOT_SATISFIABLE
+    ))
   } else if let Some(error) = r.find::<BodyDeserializeError>() {
-    Ok(warp::reply::with_status(error.to_string(), StatusCode::UNPROCESSABLE_ENTITY))
+    Ok(warp::reply::with_status(
+      error.to_string(),
+      StatusCode::UNPROCESSABLE_ENTITY
+    ))
+  } else if let Some(crate::ApiError::ExternalApiError(e)) = r.find() {
+    event!(Level::ERROR, "external api error: {}", e);
+    Ok(warp::reply::with_status(
+      "Internal server error".to_string(),
+      StatusCode::INTERNAL_SERVER_ERROR
+    ))
   } else {
-    Ok(warp::reply::with_status("Route not found".to_string(), StatusCode::NOT_FOUND))
+    Ok(warp::reply::with_status(
+      "Route not found".to_string(),
+      StatusCode::NOT_FOUND
+    ))
   }
 }
--- backend/blog_api/src/routes/question.rs
+++ backend/blog_api/src/routes/question.rs
@@ -1,3 +1,5 @@
+use error_handler::ApiLayerError;
+use serde::{Deserialize, Serialize};
 use tracing::Level;
 use tracing::{instrument, event};
 use warp::hyper::StatusCode;
@@ -5,6 +7,29 @@ use std::collections::HashMap;
 use crate::store::Store;
 use crate::types::pagination::{extract_pagination, Pagination};
 use crate::types::question::{Question, NewQuestion};
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct ApiResponse {
+  message: String
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWord {
+  original: String,
+  word: String,
+  deviations: i64,
+  info: i64,
+  #[serde(rename = "replaceLen")]
+  replace_len: i64,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct BadWordsResponse {
+  content: String,
+  bad_words_total: i64,
+  bad_words_list: Vec<BadWord>,
+  censored_content: String,
+}
 
 #[instrument]
 pub async fn get_questions(
@@ -25,12 +50,46 @@ pub async fn get_questions(
   }
 }
 
+async fn transform_error(res: reqwest::Response) -> ApiLayerError {
+  ApiLayerError {
+    status: res.status().as_u16(),
+    message: res.json::<ApiResponse>().await.unwrap().message,
+  }
+}
+
 pub async fn add_question(
   store: Store,
   new_question: NewQuestion,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-  match store.add_question(new_question).await {
-    Ok(_) => Ok(warp::reply::with_status("question added", StatusCode::OK)),
+  let client = reqwest::Client::new();
+  let res = client.post("https://api.apilayer.com/bad_words?censor_character=*")
+    .header("apikey", "Some api key")
+    .body("a list of shit words")
+    .send()
+    .await
+    .map_err(|e| error_handler::ApiError::ExternalApiError(e))?;
+
+
+  if !res.status().is_success() {
+    if res.status().is_client_error() {
+      let err = transform_error(res).await;
+      return Err(error_handler::ApiError::ClientError(err).into())
+    }
+
+    let err = transform_error(res).await;
+    return Err(error_handler::ApiError::ServerError(err).into())
+  }
+
+  let res = res.json::<BadWordsResponse>().await.map_err(|e| error_handler::ApiError::ExternalApiError(e))?;
+
+  let question = NewQuestion {
+    title: new_question.title,
+    content: res.censored_content,
+    tags: new_question.tags
+  };
+
+  match store.add_question(question).await {
+    Ok(question) => Ok(warp::reply::json(&question)),
     Err(e) => Err(warp::reject::custom(e))
   }
 }
