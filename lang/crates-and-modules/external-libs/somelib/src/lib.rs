de3604171a02c6e7d74059823921cfd6b0d04602
KPMGE <jesuskevin254@gmail.com>
2023-01-15 18:35:14 -0300

    :rocket: add example with external libs

---
+++ lang/crates-and-modules/external-libs/somelib/src/lib.rs
@@ -0,0 +1,6 @@
+// this is the place where we expose our public api for the user. 
+// anything markes as pub in here can be accessed outside.
+
+pub fn some_lib_print() {
+  println!("printing from somelib");
+}
---
+++ lang/crates-and-modules/external-libs/src/main.rs
@@ -0,0 +1,5 @@
+use somelib;
+
+fn main() {
+  somelib::some_lib_print();
+}
