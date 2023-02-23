85575483fdbdc1e76b51088108e3e0029f2c29d5
KPMGE <jesuskevin254@gmail.com>
2023-02-22 21:05:14 -0300

    :sparkles: add example with generic functions with traits

---
+++ lang/traits/generic-functions.rs
@@ -0,0 +1,30 @@
+// another way of writing polymorphic code in rust is using generic functions, the idea is pretty
+// simple, we can enforce that the type our function is gonna be applied to implements one or more
+// traits, this allows us to describe exactly the behavior we need from those functions or types.
+
+// besides the standard prelude, we need to explicitly import the traits we wanna use. this makes 
+// naming conflicts very unlikely
+use std::fmt::Debug;
+
+// this function takes a type that implements the Debug trait
+fn display_top_2_elements<T: Debug>(v: &Vec<T>) {
+    for i in 0..2 {
+        println!("{:?}", v[i]);
+    }
+}
+
+// we can specify more than one bound using the + operator, or using the where keyword
+fn run_query<M: Mapper + Serialize, R: Reducer + Serialize>(data: &DataSet, map: M, reduce: R) -> Results {
+    // ...
+}
+
+fn run_query<M, R>(data: &DataSet, map: M, reduce: R) -> Results
+    where M: Mapper + Serialize,
+          R: Reducer + Serialize {
+
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+    display_top_2_elements(&v);
+}
